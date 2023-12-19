4f390c8b45012cb239b34d165f53ba0dd5fdd7dd
ChandrasekarPerumal <chandruselv@gmail.com>
2023-12-19 19:52:58 +0530

    CastVote - Completed

--- CandidateVoting/src/main/java/com/candidate/vote/controller/CandidateController.java
+++ CandidateVoting/src/main/java/com/candidate/vote/controller/CandidateController.java
@@ -3,6 +3,7 @@ package com.candidate.vote.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,29 +39,39 @@ public class CandidateController {
 	@PutMapping("/castvote")
 	public ResponseEntity<?> updateCandidate(@RequestParam("name") String candidateName) {
 
-		
-		// ---- JSON OBject to return the data
-		
-		
-		// Check whether candidate name is already in the Map variable
-		if (candidateData_Map.containsKey(candidateName)) {
-			// Update the vote
-			candidateData_Map.put(candidateName, candidateData_Map.get(candidateName) + 1);
-		} else {
-			return new ResponseEntity<>("No Such Candidate Exists ", HttpStatus.NOT_ACCEPTABLE);
-		}
+		try {
+			candidateName = candidateName.trim();
+			if (candidateName != null && !candidateName.isBlank()) {
+				// JSON response object declaration
+				JSONObject jsonResponseObject = new JSONObject();
 
-		return new ResponseEntity<>(candidateData_Map.get(candidateName), HttpStatus.ACCEPTED);		
+				// Check whether candidate name is already in the Map variable
+				if (candidateData_Map.containsKey(candidateName)) {
+				
+					// Update the vote
+					candidateData_Map.put(candidateName, candidateData_Map.get(candidateName) + 1);
+					
+					// Create the json data for valid candidate - Contains incremented value.
+					jsonResponseObject.put("status", "Success");
+					jsonResponseObject.put("vote", candidateData_Map.get(candidateName));
+
+				} else {
+					return new ResponseEntity<>("No Such Candidate Exists", HttpStatus.NOT_FOUND);
+				}
+				return new ResponseEntity<>(jsonResponseObject, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>("Name Can't be NULL", HttpStatus.NOT_ACCEPTABLE);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.EXPECTATION_FAILED);
+		}
 	}
-	
-	
+
 	@GetMapping("/countvote")
 	public ResponseEntity<?> getCandidateVote(@RequestParam("name") String candidateName) {
 
-		
 		// ---- JSON OBject to return the data
-		
-		
+
 		// Check whether candidate name is already in the Map variable
 		if (candidateData_Map.containsKey(candidateName)) {
 			// Update the vote
@@ -70,49 +81,41 @@ public class CandidateController {
 		}
 
 		return new ResponseEntity<>(candidateData_Map.get(candidateName), HttpStatus.ACCEPTED);
-		
+
 	}
 
-	
 	@GetMapping("/listvote")
 	public ResponseEntity<?> getAllCandidate() {
 
-		
 		// ---- JSON OBject to return the data
-		
-		
+
 		// Check whether candidate name is already in the Map variable
 		if (candidateData_Map.isEmpty()) {
 			// Update the vote
-			
+
 		} else {
 			return new ResponseEntity<>("No Such Candidate Exists ", HttpStatus.NOT_ACCEPTABLE);
 		}
 
 		return new ResponseEntity<>("", HttpStatus.ACCEPTED);
-		
+
 	}
-	
-	
-	
+
 	@GetMapping("/getwinner")
 	public ResponseEntity<?> getWinnerCandidate() {
 
-		
 		// ---- JSON OBject to return the data
-		
-		
+
 		// Check whether candidate name is already in the Map variable
 		if (candidateData_Map.isEmpty()) {
 			// Update the vote
-			
+
 		} else {
 			return new ResponseEntity<>("No Such Candidate Exists ", HttpStatus.NOT_ACCEPTABLE);
 		}
 
 		return new ResponseEntity<>("", HttpStatus.ACCEPTED);
-		
-	}
 
+	}
 
 }
